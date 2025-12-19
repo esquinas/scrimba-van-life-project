355a9bed61d50f5f5ce975fd2b3e5fb01c403730
Enrique Esquinas <enrique.esquinas@marsbased.com>
2025-12-19 14:18:48 +0100

    feat: add van details

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -2,7 +2,36 @@ import React from "react"
 import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
-    const { id } = useParams();
+    const params = useParams();
+    const [van, setVan] = React.useState(null)
 
-    return <h1>Van detail page goes here {id}</h1>
+    React.useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => setVan(data.vans))
+    }, [params.id])
+
+    return (
+        <div className="van-detail-container">
+            {
+                van ? (
+                    <div className="van-detail">
+                        <img src={van.imageUrl} />
+                        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                        <h2>{van.name}</h2>
+                        <p className="van-price"><span>${van.price}</span>/day</p>
+                        <p>{van.description}</p>
+                        <button className="link-button">Rent this van</button>
+                    </div>
+                ) : <div className="van-detail">
+                        <img src="#" style={{ width: '100%', height: "300px" }} />
+                        <i className="van-type selected">&nbsp;</i>
+                        <h2>&nbsp;</h2>
+                        <p className="van-price"><span>&nbsp;</span></p>
+                        <p>Loading...</p>
+                        <button className="link-button" disabled>...</button>
+                    </div>
+            }
+        </div>
+    )
 }
