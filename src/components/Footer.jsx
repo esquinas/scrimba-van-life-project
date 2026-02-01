dfcabff85b993a6a72e60ce9d508d2948637496b
Enrique Esquinas <enrique.esquinas@marsbased.com>
2026-02-01 22:12:56 +0100

    feat: add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from "react-router-dom"
+
+export default function Footer(_props) {
+  return (
+    <footer>&#169; {(new Date()).getFullYear()} #VANLIFE</footer>
+  )
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
-
+import Footer from "./Footer"
 
 export default function Layout() {
   return (
-    <>
+    <main className="site-wrapper">
       <Header />
       <Outlet />
-    </>
+      <Footer />
+    </main>
   )
 }
