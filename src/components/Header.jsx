c2b5940c540cdda688e2850ef28d6bfdbdb47f81
Enrique Esquinas <enrique.esquinas@marsbased.com>
2026-02-01 22:34:51 +0100

    feat(ux): mark currently active link in nav bars

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export default function Header(_props) {
   return (
     <header>
       <Link className="site-logo" to="/">#VanLife</Link>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
+        <NavLink to="/host" className={ ({isActive}) => isActive ? 'active' : null } >Host</NavLink>
+        <NavLink to="/about" className={ ({isActive}) => isActive ? 'active' : null }>About</NavLink>
+        <NavLink to="/vans" className={ ({isActive}) => isActive ? 'active' : null }>Vans</NavLink>
       </nav>
     </header>
   )
