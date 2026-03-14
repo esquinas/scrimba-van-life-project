ddc57d88723e20cd780aadc156d5cf12e6c0ce9b
Enrique Esquinas <enrique.esquinas@marsbased.com>
2026-03-14 20:53:00 +0100

    feat: add host ids to data and hosts vans routes

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,12 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-/**
- * Challenge: Wrap the contents of the "van-tile" div in a
- * Link that sends the user to `/vans/${van-id-here}`.
- */
-
-
 export default function Vans() {
     const [vans, setVans] = React.useState([])
     React.useEffect(() => {
